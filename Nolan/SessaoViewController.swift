2b470f97a399ece4da5f952e8763c8cd6aa3cf18
Mateus Nunes <nunes.mateus@pucpr.edu.br>
2019-10-25 10:36:47 -0300

    Tela Sessão

---
+++ Nolan/SessaoViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SessaoViewController.swift
+//  Nolan
+//
+//  Created by Mateus Nunes on 25/10/19.
+//  Copyright © 2019 Mateus Nunes. All rights reserved.
+//
+
+import UIKit
+
+class SessaoViewController: UIViewController {
+
+    @IBOutlet weak var imagemSessao: UIImageView!
+    @IBOutlet weak var lblTituloSessao: UILabel!
+    @IBOutlet weak var lblDificuldade: UILabel!
+    @IBOutlet weak var lblCategoria: UILabel!
+    @IBOutlet weak var lblNomeSessao: UILabel!
+    @IBOutlet weak var lblTempoDuracao: UILabel!
+    @IBOutlet weak var lblLength: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
