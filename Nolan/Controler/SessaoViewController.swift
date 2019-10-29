b3bf2b7e2e55e75883e15bcf4e7d21fc2b2c4dc5
Cristiano Correia <cristiano4861@gmail.com>
2019-10-29 09:32:30 -0300

    Organizando as pastas

--- Nolan/Controler/SessaoViewController.swift
+++ Nolan/Controler/SessaoViewController.swift
@@ -25,17 +25,4 @@ class SessaoViewController: UIViewController {
 
     }
     
-    
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
