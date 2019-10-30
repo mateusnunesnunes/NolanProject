b4c1d4de65660ab00dfc9ec295186ae11e3caf4b
Cristiano Correia <cristiano4861@gmail.com>
2019-10-30 11:26:04 -0300

    Alterações SessãoVC e FeedVC
    Delegate das TableView e CollectionView
    Elementos adicionados no Storyboard: dentro de Table e Collection
    Alterações provisórias nos layouts para perceber a UI no app.

---
+++ Nolan/Controler/PoseDescriptionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PoseDescriptionViewController.swift
+//  Nolan
+//
+//  Created by Cristiano Correia on 30/10/19.
+//  Copyright © 2019 Mateus Nunes. All rights reserved.
+//
+
+import UIKit
+
+class PoseDescriptionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Nolan/View/Extension/RoundedImage.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  Nolan
+//
+//  Created by Cristiano Correia on 30/10/19.
+//  Copyright © 2019 Mateus Nunes. All rights reserved.
+//
+
+import Foundation
