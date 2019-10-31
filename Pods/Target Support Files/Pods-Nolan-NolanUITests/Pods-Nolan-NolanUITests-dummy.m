c1ab873979e37a7cb3b5dc5a9b5c400271a65735
Enzo Maruffa <enzomm1999@gmail.com>
2019-10-31 09:47:11 -0300

    updates pods file

--- Pods/Target Support Files/Pods-Nolan-NolanUITests/Pods-Nolan-NolanUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Nolan_NolanUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Nolan_NolanUITests
-@end
--- Pods/Target Support Files/Pods-Nolan/Pods-Nolan-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Nolan : NSObject
-@end
-@implementation PodsDummy_Pods_Nolan
-@end
