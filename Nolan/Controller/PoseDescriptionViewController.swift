d5756b14bc0b7afda91233298e3a864921517b8d
Enzo Maruffa <enzomm1999@gmail.com>
2019-11-04 11:04:55 -0300

    Removes unused variables in PoseDescription

--- Nolan/Controller/PoseDescriptionViewController.swift
+++ Nolan/Controller/PoseDescriptionViewController.swift
@@ -12,9 +12,6 @@ class PoseDescriptionViewController: UIViewController, UITableViewDataSource, UI
         
     @IBOutlet weak var tableView: UITableView!
     
-    var index: Int!
-    var count = 0
-    
     var pose: Pose?
 
     override func viewDidLoad() {
@@ -37,7 +34,6 @@ class PoseDescriptionViewController: UIViewController, UITableViewDataSource, UI
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "PoseDEscription", for: indexPath) as! PoseDescriptionTableViewCell
         
-        
         cell.lblStep.text = "STEP \(indexPath.row+1)"
         cell.lblStepDescription.text = self.pose?.steps[indexPath.row]
         
@@ -45,20 +41,23 @@ class PoseDescriptionViewController: UIViewController, UITableViewDataSource, UI
     }
     
     
-    //não sei o que é isso, comenta aí quem fez!
+    // Método chamado quando row é clicado
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let singletonIndex = indexPath.row
+        // Executamos a segue - criada no Storyboard - com o  nome de viewPose enviando junto, como dado, a pose atual
         self.performSegue(withIdentifier: "viewPose", sender: pose)
     }
     
+    // O prepare é chamado  sempre que uma segue é executada
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Vejo se a segue sendo executada é a "viewPose"
         if segue.identifier == "viewPose" {
+            // Se for, checo se o destino da Segue é de fato uma ViewARPoseViewController e se consigo converter o dado recebido em uma Pose
             if let destination = segue.destination as? ViewARPoseViewController, let pose = sender as? Pose {
-                
+                // Se deu tudo certo, printo e seto no destino a pose como sendo a pose recebida por essa função - ou seja, a da view atual
                 print("Setting viewARPose file as \(pose.jsonFilename) and name \(pose.name)")
                 destination.pose = pose
-                
             } else{
+                // Se deu errado, avisa
                 print("Failed creating view OR sender")
             }
         }
